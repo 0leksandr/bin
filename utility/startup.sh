3069014d0586cb21ef489390c8497ae08c0d2991
Oleksandr Boiko <oleksandr.boiko@i.ua>
2020-07-29 00:15:21 +0300

    back to `upgrade` in `update`; `git-reset`+`scgo`

--- utility/startup.sh
+++ utility/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-/home/nezhraba/_/Programs/bin/utility/bluetooth-off
+$HOME/_/Programs/bin/utility/bluetooth-off
 mic-off
 # volume 50%
 light-mode all &
@@ -8,12 +8,12 @@ light-mode all &
 sleep 5
 # vivaldi-my &
 # systemctl restart systemd-udevd systemd-udevd-kernel.socket systemd-udevd-control.socket & # hotfix, see https://askubuntu.com/questions/1035528/ubuntu-18-04-systemd-udevd-uses-high-cpu-conflict-with-nvidia-graphics
-# /home/nezhraba/Programs/bash/battery-control.sh
+# $HOME/_/Programs/bin/battery-control.sh
 conky-my &
 sleepy 00:00
 
 sleep 5
-cronus /home/nezhraba/_/Programs/cronostab &
+cronus $HOME/_/Programs/cronostab &
 # start-closed skypeforlinux 'Skype'
 start-closed slack 'Slack( \|.*)?'
 start-closed telegram 'Telegram( \([0-9]+\))?'
@@ -21,7 +21,7 @@ start-closed telegram 'Telegram( \([0-9]+\))?'
 sleep 10
 daemons 'light-mode all' 'conky-my' 'telegram' &
 
-alert "$0 finished|$(pwd)|$HOME"
+alert "$0 finished"
 
 # for command in \
 #     "sleep 20 && daemons 'light-mode all' 'conky-my' 'telegram'" \
