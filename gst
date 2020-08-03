955717d5d45f52382e7c5b8b69b5e48ea0a8096e
Oleksandr Boiko <oleksandr.boiko@i.ua>
2020-08-03 22:31:10 +0300

    git-switch

--- gst
+++ gst
@@ -2,11 +2,14 @@
 status=$( { git status --short ;} 2>&1 )
 if [ "$status" != "" ]; then
     git status --short
+    exit 1
 else
     status_push=$(git status --short --branch |grep ahead)
     if [ "$status_push" != "" ]; then
         git status --short --branch
+        exit 1
     else
         env printf '\xF0\x9F\x91\x8D\n'
+        exit 0
     fi
 fi
--- trash/test-read-input.sh
+++ trash/test-read-input.sh
@@ -1,13 +1,41 @@
 #!/bin/sh
-read -p "Heh?" aa
-echo $aa
+if [ "" ]; then
+    read -p "Heh?" aa
+    echo $aa
 
-read void
+    read void
 
-if [ $(read aa) ]; then
-    echo "yes"
-else
-    echo "no"
+    if [ $(read aa) ]; then
+        echo "yes"
+    else
+        echo "no"
+    fi
 fi
 
-echo "exit"
+# echo "testing multiple read with timeout"
+# while :; do
+#     timeout 5s read a
+#     echo "a[$a]"
+# done
+
+while :; do
+    hr
+    timeout --foreground 5 sh -c 'read a; echo $a'|while read -r a; do
+        echo "a[$a]"
+    done
+done
+
+if [ "" ]; then
+    printf "%s\n" "Do you want to un-Mount the External Drives?"
+    timeout --foreground 10 bash -c '
+        select sel in "yes" "no"
+        do
+            echo "$REPLY"
+            break
+        done' | while read -r answer; do
+            echo ">>Got from user: $answer"
+            case "$answer" in
+                "yes") echo "Unmounting stuff" ;;
+            esac
+    done
+fi
