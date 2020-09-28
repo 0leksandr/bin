bd3a251e4e7ad3ea3018957741c2e51960431cda
Oleksandr Boiko <oleksandr.boiko@i.ua>
2020-09-28 16:41:38 +0300

    `startup` self-control; `browser-tab-key`

--- utility/startup.sh
+++ utility/startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+control=$(run-detached "sleep 35 && alert '$0 was not finished correctly'")
+
 $HOME/_/Programs/bin/utility/bluetooth-off
 mic-off
 # volume 40%
@@ -14,11 +16,11 @@ sleepy 00:00
 
 sleep 5
 cronus $HOME/_/Programs/cronostab &
-# start-closed skypeforlinux 'Skype'
+start-closed skypeforlinux 'Skype'
 start-closed slack 'Slack( \|.*)?'
 start-closed telegram 'Telegram( \([0-9]+\))?'
 
 sleep 10
-daemons 'light-mode all' 'conky-my' 'telegram' 'cronus' 'close-browser-tab' &
+daemons 'light-mode all' 'conky-my' 'telegram' 'cronus' 'browser-tab-key' &
 
-alert "$0 finished"
+fkill "$control"
