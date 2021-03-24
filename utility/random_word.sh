64075a69187847669efb58f3cd419f131a29ab0d
Oleksandr Boiko <oleksandr.boiko@i.ua>
2021-03-24 14:02:42 +0200

    `_rand`, `random_word.sh`

---
+++ utility/random_word.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dics_dir="/home/nezhraba/_/Parallango/dics"
+dic_file="${dics_dir}/ukr/ukr.txt"
+nr_lines="$(wc -l ${dic_file} |sed -r 's/^([0-9]+) .*$/\1/')"
+line_nr=$(_rand $nr_lines)
+line="$(sed "${line_nr}q;d" ${dic_file})"
+word="$(echo "${line}" |sed -r 's ^([^|]+)\|.*$ \1 ')"
+echo $word
