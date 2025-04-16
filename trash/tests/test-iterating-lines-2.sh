f9297d708615fbb04980a8127bf8a06fabfcb921
Oleksandr Boiko <boiol.gokhm@gmail.com>
2025-04-16 13:05:07 +0100

    WIP git-submodules and other

--- trash/tests/test-iterating-lines-2.sh
+++ trash/tests/test-iterating-lines-2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-lines="  line1\n  line2"
+lines="  line1\n  line2 line2-2 "
 
 for line in $lines; do
     echo "1 line[$line]"
 done
 
-echo "$lines" |while read line; do
+echo "$lines" |while read -r line; do
     echo "2 line[$line]"
 done
 echo "done script"
