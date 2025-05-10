c172d5acd8963943908bc5972abf1242a16078dc
Oleksandr Boiko <boiol.gokhm@gmail.com>
2025-05-10 14:30:24 +0100

    .

--- utils/system-server-scripts/browser-key.sh
+++ utils/system-server-scripts/browser-key.sh
@@ -6,21 +6,45 @@ delay="$2"
 
 #list="$(vivaldi-windows list)"
 
+os="$(_os)"
+
 if [ $# -ge 3 ]; then
     keys=""
     i=0
     for key in "$@"; do
         i=$(($i + 1))
         if [ $i -ge 3 ]; then
-            keys="$keys key $key"
+            case "$os" in
+                linux) keys="$keys key $key" ;;
+                mac)   keys="$keys$key"      ;;
+            esac
         fi
     done
     if [ $delay -gt 0 ]; then sleep "$delay"; fi
-    if [ "$title" ]; then
-        xdotool search --name "^$title - Vivaldi$" windowactivate --sync %1 $keys windowactivate $(xdotool getactivewindow)
-    else
-        xdotool $keys
-    fi
+
+    case "$os" in
+        linux)
+            if [ "$title" ]; then
+                xdotool search --name "^$title - Vivaldi$" windowactivate --sync %1 $keys windowactivate $(xdotool getactivewindow)
+            else
+                xdotool $keys
+            fi
+            ;;
+        mac)
+            #if [ ! "$title" ]; then
+            #    title="$(lsappinfo info "$(lsappinfo front)" |head --lines=1 |sed -r 's/^"([^"]+)".*$/\1/')"
+            #fi
+            if [ "$title" ]; then
+                osascript -e "activate application \"$title\""
+            fi
+            case "$keys" in
+                "ctrl+"*) keystroke="$(echo "$keys" |sed -r 's/^ctrl\+(.+)$/\1/') using command down" ;;
+                *)        keystroke="$keys"
+            esac
+notify "keystroke[$keystroke]"
+            osascript -e "tell application \"System Events\" to keystroke \"$keystroke\""
+            ;;
+    esac
 else
     alert "$0: error"
 fi
